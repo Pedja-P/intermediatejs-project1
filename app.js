f211c7dc9b03401bf6ea76382a06c008c5386fde
Predrag Pavlovic <predrag.pavlovic@icloud.com>
2020-12-09 04:09:41 +0100

    feat: Add Dinosaur class

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-    // Create Dino Constructor
+// Create Dino Constructor
+class Dinosaur {
+    constructor({ species, weight, height, diet, where, when, fact }) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
+}
 
+// Create Dino Objects
 
-    // Create Dino Objects
+// Create Human Object
 
+// Use IIFE to get human data from form
 
-    // Create Human Object
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Generate Tiles for each Dino in Array
 
+// Add tiles to DOM
 
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic

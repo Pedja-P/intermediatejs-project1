ba50b9f6ac3c7c80d56a43b1811856652e93a2d1
Predrag Pavlovic <predrag.pavlovic@icloud.com>
2020-12-12 23:33:18 +0100

    feat: Create dinos and human objects

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Create Dino Constructor
 class Dinosaur {
     constructor({ species, weight, height, diet, where, when, fact }) {
@@ -12,10 +14,44 @@ class Dinosaur {
 }
 
 // Create Dino Objects
+let dinosaurs = [];
+
+const getDinoData = async () => {
+    const fetchedData = await fetch('./dino.json');
+    const data = await fetchedData.json();
+    return data.Dinos;
+};
+
+window.onload = async () => {
+    const dinoData = await getDinoData();
+    dinosaurs = dinoData.map((dino) => new Dinosaur(dino));
+    console.log(dinosaurs);
+};
 
 // Create Human Object
+class Human {
+    constructor({ name, weight, height, diet }) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+    }
+}
 
 // Use IIFE to get human data from form
+document.getElementById('btn').addEventListener('click', function () {
+    const humanData = (() => {
+        const form = document.getElementById('dino-compare');
+        return {
+            name: form.name.value,
+            height: Number(form.feet.value) * 12 + Number(form.inches.value),
+            weight: Number(form.weight.value),
+            diet: form.diet.value,
+        };
+    })();
+    const human = new Human(humanData);
+    console.log(human);
+});
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
